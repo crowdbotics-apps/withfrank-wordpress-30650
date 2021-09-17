aa3d56bc4db55ec081a57740d07b736dc882d7b6
Crowdbotics <team@crowdbotics.com>
2021-09-17 19:36:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "withfrank_wordpress_30650Identifier",
+  url: "https://withfrank_wordpress_30650Identifier.botics.co",
+  api: "https://withfrank_wordpress_30650Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
